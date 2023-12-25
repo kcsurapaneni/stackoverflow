23b59585146c4523767e288857bab2829c3c0ff7
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-12-25 15:38:12 -0700

    CustomSecurityFilter deleted and Security Configuration updated accordingly

--- security/src/main/java/com/learn/security/config/SecurityConfiguration.java
+++ security/src/main/java/com/learn/security/config/SecurityConfiguration.java
@@ -1,13 +1,11 @@
 package com.learn.security.config;
 
-import com.learn.security.filter.*;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.config.annotation.web.configurers.*;
 import org.springframework.security.web.*;
-import org.springframework.security.web.authentication.www.*;
 
 /**
  * @author Krishna Chaitanya
@@ -20,15 +18,14 @@ public class SecurityConfiguration {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            .cors(AbstractHttpConfigurer::disable)
-            .httpBasic(Customizer.withDefaults())
-            .authorizeHttpRequests(
-                    httpRequests -> httpRequests
-                            .requestMatchers("/error").permitAll()
-                            .anyRequest().authenticated()
-            )
-            .addFilterBefore(new CustomSecurityFilter(), BasicAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        httpRequests -> httpRequests
+                                .requestMatchers("/error").permitAll()
+                                .anyRequest().authenticated()
+                )
         ;
 
         return http.build();
